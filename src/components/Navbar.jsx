56405bfa3c215368b0604ec93964d10aa89702a0
Sumit Kumar Jha <jhasumit4742+github@gmail.com>
2024-06-10 23:19:06 +0530

    Made the Preview page responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import toast from 'react-hot-toast'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     
-    <div id="navWrapper" className='bg-white flex justify-between w-11/12 p-2 rounded-md px-5'>
+    <div id="navWrapper" className='bg-white flex flex-col sm:flex-row justify-center items-center sm:justify-between gap-3 sm:gap-0 w-11/12 p-2 rounded-md px-5'>
           <Link to="/"><button id="backToEditorButton" 
           className='p-3 border-2 rounded-md border-purply text-base text-purply w-40
             hover:bg-purply hover:text-white
           '> Back to Editor</button> </Link>
           <button id="shareLinkButton"
-          className='ml-2 p-3 border-2 rounded-md text-base bg-purply w-40 text-white
+          className='sm:ml-2 p-3 border-2 rounded-md text-base bg-purply w-40 text-white
             hover:border-purply hover:bg-white hover:text-purply
-          '>
+          '
+          onClick={()=>{toast.success("Thanks for using Devlinks! We are working to bring Sharing Feature soon!")}}>
                 Share Link
           </button>
     </div>
--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -6,8 +6,8 @@ const Preview = () => {
   return (
     <>
     <div id='profileViewContainer' className='h-screen w-full'>
-      <div id="profileViewWrapper" className='bg-purply h-36% rounded-b-3xl'>
-        <div className='pl-24 pt-4 '>
+      <div id="profileViewWrapper" className='sm:bg-purply h-36% rounded-b-3xl'>
+        <div className='sm:pl-24 pt-4 '>
             <Navbar/>
             <Card/>
         </div>
